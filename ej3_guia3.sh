bc36d6961d5d9fd64676ee7296affc8c0d42175f
LucianoAD <35845435+LucianoAD@users.noreply.github.com>
2022-12-19 16:28:59 -0300

    sugerencias

--- ej3_guia3.sh
+++ ej3_guia3.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-BUSCADOR=$(ps aux| grep -wE "chrome|firefox|opera|brave"|wc -l)
+BUSCADOR=$(ps aux| grep -wE "chrome|firefox|opera|brave"|wc -l) # podes usar BUSCADOR=$(ps aux| pgrep "chrome|firefox|opera|brave"|wc -l) para que no machee con
+## el proceso grep, sino simpre arroja "true"
+
 if [[ $BUSCADOR > 0 ]]
 then
 	echo "true"
