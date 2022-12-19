9234f5dd476ec74a6dfc63392ddd7012312f9902
LucianoAD <35845435+LucianoAD@users.noreply.github.com>
2022-12-19 13:12:21 -0300

    Observaciones hello.sh

--- scripts/guia1/hello.sh
+++ scripts/guia1/hello.sh
@@ -1,1 +1,4 @@
-hello world
+#!/bin/bash
+echo "Hello world"
+
+## Recordar que en los scripts de shell siempre se escribe en la pripmera linea la direccion al interprete de comando, en este caso bash
